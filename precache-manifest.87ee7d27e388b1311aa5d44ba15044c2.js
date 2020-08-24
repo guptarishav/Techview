93d9a166abd80e33bb9fd66838c331674e7d026e
Rishav <kumar.164@iitj.ac.in>
2020-08-24 15:06:34 +0530

    Updates

---
+++ precache-manifest.87ee7d27e388b1311aa5d44ba15044c2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69c484e133ca8e76d97a5703622e7e7",
+    "url": "/Techview/index.html"
+  },
+  {
+    "revision": "a931127e994b1d132420",
+    "url": "/Techview/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "a08a4217aed29c141ae7",
+    "url": "/Techview/static/css/main.59e4f258.chunk.css"
+  },
+  {
+    "revision": "a931127e994b1d132420",
+    "url": "/Techview/static/js/2.c8549d12.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/Techview/static/js/2.c8549d12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a08a4217aed29c141ae7",
+    "url": "/Techview/static/js/main.05a2a67b.chunk.js"
+  },
+  {
+    "revision": "0ede10e6ea9d794d3937",
+    "url": "/Techview/static/js/runtime-main.bb8b44f4.js"
+  },
+  {
+    "revision": "59b711e1241e13c74066a5fff93993f5",
+    "url": "/Techview/static/media/android.59b711e1.png"
+  },
+  {
+    "revision": "647d7e1378a56bc3318cbf38279895fd",
+    "url": "/Techview/static/media/backend.647d7e13.jpg"
+  },
+  {
+    "revision": "4ea8a614aa66ebe9431dc0ef3b1e6f0e",
+    "url": "/Techview/static/media/frontend.4ea8a614.jpg"
+  },
+  {
+    "revision": "d9a2e76d40f0dc1c744b015e99def29a",
+    "url": "/Techview/static/media/home.d9a2e76d.png"
+  },
+  {
+    "revision": "ef178b6ad6c98574b4b99d35eb595e33",
+    "url": "/Techview/static/media/img2.ef178b6a.jpg"
+  },
+  {
+    "revision": "a91533eaef334cb3dd8c92e5a03afd09",
+    "url": "/Techview/static/media/management.a91533ea.png"
+  },
+  {
+    "revision": "86bef79557f4ea4571fe63f22b0def52",
+    "url": "/Techview/static/media/software-developer.86bef795.png"
+  }
+]);
